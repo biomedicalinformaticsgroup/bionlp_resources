f27eaef2d1f5e07e2afc23dfdad4bcfbc7cd46fe
s2010515 <s2010515@ed.ac.uk>
2022-04-16 16:18:47 +0000

    Update code

--- bionlp_resources/main/__init__.py
+++ bionlp_resources/main/__init__.py
@@ -3,3 +3,4 @@ from bionlp_resources.main.cui_to_sab import cui_to_sab
 from bionlp_resources.main.pmc_oa_generation import pmc_oa_generation
 from bionlp_resources.main.tuis_to_semantics import tuis_to_semantics
 from bionlp_resources.main.pygnormplus import pygnormplus
+from bionlp_resources.main.pytmvar import pytmvar
